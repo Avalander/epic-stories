6482e111cf22345dc244c44140a2067ff9fd35f5
Avalander <darth.quesorico@gmail.com>
2018-03-20 22:57:50 +0100

    Create store for stories.

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const loginUser = (req, res) => ({ token, user }) =>
 	res.cookie('bearer', token, { httpOnly: true })
 		.json({ username: user.username, groups: user.groups })
 
-module.exports = ({ Router, signIn, authorise, registerUser }) => {
+module.exports = ({ Router, signIn, authorise, registerUser, createStory, findAllStories, findStory }) => {
 	const api = Router()
 
 	api.post('/register/:token', (req, res, next) => {
@@ -24,6 +24,24 @@ module.exports = ({ Router, signIn, authorise, registerUser }) => {
 	})
 
 	api.post('/ping', authorise, (req, res) => res.json({ message: 'pong' }))
+
+	api.get('/stories', (req, res, next) => findAllStories(1)
+		.then(stories => res.json(stories))
+		.catch(next)
+	)
+
+	api.get('/stories/:id', (req, res, next) => findStory(req.params.id)
+		.then(story => res.json(story))
+		.catch(next)
+	)
+
+	api.post('/stories', (req, res, next) => {
+		const { title, description, group } = req.body
+		if (!title || !group) return next(new Error('Missing information.'))
+		createStory({ title, description, group })
+			.then(({ insertedId }) => res.json({ insertedId }))
+			.catch(next)
+	})
 
 	return api
 }
