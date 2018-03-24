3100936adeb1de2a29ef5b08957857b30555f546
Avalander <darth.quesorico@gmail.com>
2018-03-24 12:25:30 +0100

    Fix start script to not spawn a child process.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting server..."
-npm start >> default.log 2>&1 &
+NODE_PATH=server:shared node server/index.js >> default.log 2>&1 &
 PID=$!
 echo $PID > "pid.txt"
 echo "Server started [pid: $PID]"
