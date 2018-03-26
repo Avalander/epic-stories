9733fb195e940d75fffa564aa08c2a48e5113df9
Avalander <darth.quesorico@gmail.com>
2018-03-26 13:53:03 +0200

    Replaced custom fetch and post login in story/index.js.

--- web/app/http.js
+++ web/app/http.js
@@ -1,3 +1,6 @@
+import xs from 'xstream'
+
+
 export const makeFetch = (HTTP, category, url$) => {
 	const request$ = url$.map(url => ({
 		url,
@@ -18,5 +21,29 @@ export const makeFetch = (HTTP, category, url$) => {
 		request$,
 		response$,
 		error$,
+	}
+}
+
+export const makePost = (HTTP, category, url$) => {
+	const http_response$ = HTTP.select(category)
+		.flatten()
+		.map(req => req.body)
+	const response$ = http_response$.filter(res => res.ok)
+		.map(res => res.result)
+	const error$ = http_response$.filter(res => !res.ok)
+		.map(res => res.error)
+	
+	const makeRequest = data$ => xs.combine(url$, data$)
+		.map(([ url, data ]) => ({
+			url, category,
+			method: 'POST',
+			withCredentials: true,
+			send: data,
+		}))
+	
+	return {
+		response$,
+		error$,
+		makeRequest,
 	}
 }
