80eb2e822a4f8c331391ebd1701378e118920de9
Avalander <darth.quesorico@gmail.com>
2018-03-28 20:04:03 +0200

    Add parse bold to story view.

--- web/app/__test__/syntax.test.js
+++ web/app/__test__/syntax.test.js
@@ -32,13 +32,13 @@ test('textToVdom should remove empty lines.', t => {
 	t.end()
 })
 
-test('textToVdom should put text wrapped in * within strong tags.', t => {
+test('textToVdom should put text wrapped in * within span.bold tags.', t => {
 	const text = 'This should be *bold* text.'
 
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><span>This should be </span><strong>bold</strong><span> text.</span></p>')
+	htmlLooksLike(t, actual[0], '<p><span>This should be </span><span class="bold">bold</span><span> text.</span></p>')
 
 	t.end()
 })
@@ -49,7 +49,7 @@ test('textToVdom should not add an empty span at the beginning.', t => {
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><strong>This</strong><span> should be bold.</span></p>')
+	htmlLooksLike(t, actual[0], '<p><span class="bold">This</span><span> should be bold.</span></p>')
 
 	t.end()
 })
@@ -60,7 +60,7 @@ test('textToVdom should not add an empty span at the end.', t => {
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><span>This should be </span><strong>bold.</strong></p>')
+	htmlLooksLike(t, actual[0], '<p><span>This should be </span><span class="bold">bold.</span></p>')
 
 	t.end()
 })
@@ -71,13 +71,49 @@ test('textToVdom should parse multiple bold items.', t => {
 	const actual = textToVdom(text)
 
 	const expected = `<p>
-		<strong>Bold</strong>
+		<span class="bold">Bold</span>
 		<span> not bold </span>
-		<strong>more bold</strong>
+		<span class="bold">more bold</span>
 		<span> potato.</span>
 	</p>`
 	t.equal(actual.length, 1)
 	htmlLooksLike(t, actual[0], expected)
+
+	t.end()
+})
+
+test('textToVdom should not consider * in different lines when parsing bold.', t => {
+	const text = `This is *some
+	serious* stuff`
+
+	const actual = textToVdom(text)
+	
+	t.equal(actual.length, 2)
+	htmlLooksLike(t, actual[0], '<p>This is *some</p>')
+	htmlLooksLike(t, actual[1], '<p>serious* stuff</p>')
+
+	t.end()
+})
+
+test('textToVdom should parse bold items separated with a period correctly.', t => {
+	const text = '*Bold*. *Bold*'
+
+	const actual = textToVdom(text)
+
+	const expected = '<p><span class="bold">Bold</span><span>. </span><span class="bold">Bold</span></p>'
+	t.equal(actual.length, 1)
+	htmlLooksLike(t, actual[0], expected)
+
+	t.end()
+})
+
+test.skip('textToVdom should put text wrapped in _ within span.italic tags.', t => {
+	const text = '_Italics_ normal.'
+
+	const actual = textToVdom(text)
+
+	t.equal(actual.length, 1)
+	htmlLooksLike(t, actual[0], '<p><span class="italic">Italics</span><span> normal.</span></p>')
 
 	t.end()
 })
--- web/app/syntax.js
+++ web/app/syntax.js
@@ -8,6 +8,8 @@ import {
 
 const bold_regex = /\*([\w|\.][^\n\*]*[\w|\.])\*/
 
+const regular = text => span('', text)
+const bold = text => span('.bold', text)
 
 const addIfNotEmpty = (arr, str, elem) => (str.length > 0 ? arr.push(elem(str)) : {})
 
@@ -16,12 +18,12 @@ const parseBold = text => {
 	let rest = text
 	let match = bold_regex.exec(rest)
 	while (match !== null) {
-		addIfNotEmpty(result, match.input.substring(0, match.index), span)
-		addIfNotEmpty(result, match[1], strong)
+		addIfNotEmpty(result, match.input.substring(0, match.index), regular)
+		addIfNotEmpty(result, match[1], bold)
 		rest = match.input.substring(match.index + match[0].length)
 		match = bold_regex.exec(rest)
 	}
-	addIfNotEmpty(result, rest, span)
+	addIfNotEmpty(result, rest, regular)
 	return result.length > 1 ? result : text
 }
 
