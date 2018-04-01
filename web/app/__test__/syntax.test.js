7ca7a359e40dee7431e21a5e40711a1ac99e3e8f
Avalander <darth.quesorico@gmail.com>
2018-04-01 18:53:42 +0200

    Feature/parse italics (#15)
    * Refactor syntax.js to use a pseudo syntax tree.
    
    * Parse italics.
    
    * Parse strikethrough.

--- web/app/__test__/syntax.test.js
+++ web/app/__test__/syntax.test.js
@@ -12,8 +12,8 @@ test('textToVdom should break lines into paragraphs.', t => {
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 2)
-	htmlLooksLike(t, actual[0], '<p>This is paragraph one.</p>')
-	htmlLooksLike(t, actual[1], '<p>This is paragraph two.</p>')
+	htmlLooksLike(t, actual[0], '<p><span>This is paragraph one.</span></p>')
+	htmlLooksLike(t, actual[1], '<p><span>This is paragraph two.</span></p>')
 
 	t.end()
 })
@@ -26,19 +26,19 @@ test('textToVdom should remove empty lines.', t => {
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 2)
-	htmlLooksLike(t, actual[0], '<p>This is paragraph one.</p>')
-	htmlLooksLike(t, actual[1], '<p>This is paragraph two.</p>')
+	htmlLooksLike(t, actual[0], '<p><span>This is paragraph one.</span></p>')
+	htmlLooksLike(t, actual[1], '<p><span>This is paragraph two.</span></p>')
 
 	t.end()
 })
 
-test('textToVdom should put text wrapped in * within span.bold tags.', t => {
+test('textToVdom should put text wrapped in * within strong tags.', t => {
 	const text = 'This should be *bold* text.'
 
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><span>This should be </span><span class="bold">bold</span><span> text.</span></p>')
+	htmlLooksLike(t, actual[0], '<p><span>This should be </span><strong>bold</strong><span> text.</span></p>')
 
 	t.end()
 })
@@ -49,7 +49,7 @@ test('textToVdom should not add an empty span at the beginning.', t => {
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><span class="bold">This</span><span> should be bold.</span></p>')
+	htmlLooksLike(t, actual[0], '<p><strong>This</strong><span> should be bold.</span></p>')
 
 	t.end()
 })
@@ -60,7 +60,7 @@ test('textToVdom should not add an empty span at the end.', t => {
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><span>This should be </span><span class="bold">bold.</span></p>')
+	htmlLooksLike(t, actual[0], '<p><span>This should be </span><strong>bold.</strong></p>')
 
 	t.end()
 })
@@ -71,9 +71,9 @@ test('textToVdom should parse multiple bold items.', t => {
 	const actual = textToVdom(text)
 
 	const expected = `<p>
-		<span class="bold">Bold</span>
+		<strong>Bold</strong>
 		<span> not bold </span>
-		<span class="bold">more bold</span>
+		<strong>more bold</strong>
 		<span> potato.</span>
 	</p>`
 	t.equal(actual.length, 1)
@@ -89,8 +89,8 @@ test('textToVdom should not consider * in different lines when parsing bold.', t
 	const actual = textToVdom(text)
 	
 	t.equal(actual.length, 2)
-	htmlLooksLike(t, actual[0], '<p>This is *some</p>')
-	htmlLooksLike(t, actual[1], '<p>serious* stuff</p>')
+	htmlLooksLike(t, actual[0], '<p><span>This is *some</span></p>')
+	htmlLooksLike(t, actual[1], '<p><span>serious* stuff</span></p>')
 
 	t.end()
 })
@@ -100,20 +100,57 @@ test('textToVdom should parse bold items separated with a period correctly.', t 
 
 	const actual = textToVdom(text)
 
-	const expected = '<p><span class="bold">Bold</span><span>. </span><span class="bold">Bold</span></p>'
+	const expected = '<p><strong>Bold</strong><span>. </span><strong>Bold</strong></p>'
 	t.equal(actual.length, 1)
 	htmlLooksLike(t, actual[0], expected)
 
 	t.end()
 })
 
-test.skip('textToVdom should put text wrapped in _ within span.italic tags.', t => {
+test('textToVdom should put text wrapped in _ within em tags.', t => {
 	const text = '_Italics_ normal.'
 
 	const actual = textToVdom(text)
 
 	t.equal(actual.length, 1)
-	htmlLooksLike(t, actual[0], '<p><span class="italic">Italics</span><span> normal.</span></p>')
+	htmlLooksLike(t, actual[0], '<p><em>Italics</em><span> normal.</span></p>')
+
+	t.end()
+})
+
+test('textToVdom should parse italics and bold.', t => {
+	const text = 'Some _text_ is _italics and some_ text *is* _bold_. *Or* _bolder_'
+
+	const actual = textToVdom(text)
+
+	const expected = `<p>
+		<span>Some </span>
+		<em>text</em>
+		<span> is </span>
+		<em>italics and some</em>
+		<span> text </span>
+		<strong>is</strong>
+		<span> </span>
+		<em>bold</em>
+		<span>. </span>
+		<strong>Or</strong>
+		<span> </span>
+		<em>bolder</em>
+	</p>`
+
+	t.equal(actual.length, 1)
+	htmlLooksLike(t, actual[0], expected)
+
+	t.end()
+})
+
+test('textToVdom should put text between ~ within del tags.', t => {
+	const text = '~Some strikethrough~'
+
+	const actual = textToVdom(text)
+
+	t.equal(actual.length, 1)
+	htmlLooksLike(t, actual[0], '<p><del>Some strikethrough</del></p>')
 
 	t.end()
 })
