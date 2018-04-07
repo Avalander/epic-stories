a3512ab1bae4f063d16a7cf3186fa2d86cd7f520
Avalander <darth.quesorico@gmail.com>
2018-04-07 17:49:09 +0200

    Remove unused code.

--- server/validators/index.js
+++ server/validators/index.js
@@ -7,17 +7,6 @@ const exists = obj => obj !== undefined && obj !== null
 
 const hasContent = value => exists(value) && (typeof value === 'string' ? value.length > 0 : true)
 
-const makePromiseValidator = (required_keys, optional_keys) => obj => {
-	const expected_keys = [ ...required_keys, ...optional_keys ]
-	for (let key of required_keys) {
-		if (!hasContent(obj[key])) return Promise.reject(`Missing key '${key}'.`)
-	}
-	for (let key of Object.keys(obj)) {
-		if (!expected_keys.includes(key)) return Promise.reject(`Unexpected key '${key}'.`)
-	}
-	return Promise.resolve(obj)
-}
-
 const makeValidator = (required_keys, optional_keys) => obj => {
 	const expected_keys = [ ...required_keys, ...optional_keys ]
 	for (let key of required_keys) {
