2391c96ca9c288434e9edb04cba1f2a443940d87
Avalander <darth.quesorico@gmail.com>
2018-09-22 18:06:38 +0200

    Add e2e tests for editing posts.

--- cypress/integration/stories/posts/create.spec.js
+++ cypress/integration/stories/posts/create.spec.js
@@ -55,9 +55,48 @@ describe('Create post', () => {
 			.should('contain', `Missing key 'text'`)
 	})
 
+	it('Adds new post to the end of the list', () => {
+		prepareStories()
+			.then(story_id => {
+				preparePosts(story_id)
+				cy.visit(urls.stories.posts(story_id, 1))
+				cy.get('#reply-btn')
+					.click()
+				cy.get('#edit-text')
+					.type('This should be third post.')
+				cy.get('#edit-save-btn')
+					.click()
+				postsArePresent([
+					[ 'First post.', 'test', 1 ],
+					[ 'Second post.', 'batman', 2 ],
+					[ 'This should be third post.', 'test', 3 ],
+				])
+			})
+	})
+
+	function postsArePresent(posts) {
+		cy.wrap(posts)
+			.each(([ text, author, position ]) => {
+				cy.get(`:nth-child(${position}) > .post-body`)
+					.should('contain', text)
+					.find('.post-body-header')
+						.should('contain', author)
+			})
+	}
+
 	function prepareStories() {
 		return cy.fixture('stories/story.list.json')
 			.then(cy.$createStories)
 			.then(ids => ids[0])
+	}
+
+	function preparePosts(story_id) {
+		return cy.fixture('/stories/post.list.json')
+			.then(posts =>
+				posts.map(
+					x => ({ ...x, story_id })
+				)
+			)
+			.then(posts => cy.$insertInDb('posts', posts))
 	}
 })
