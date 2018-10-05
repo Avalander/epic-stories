afba11fa331e72eaf80220de43667e8d4196f176
Avalander <darth.quesorico@gmail.com>
2018-10-05 20:59:57 +0200

    Moved error reporting to index.html.

--- web/app/main.js
+++ web/app/main.js
@@ -15,40 +15,6 @@ import {
 } from 'App/pages'
 
 
-window.onerror = (message, source, lineno, colno, error) => {
-	fetch('/api/report/error', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			message,
-			source,
-			lineno,
-			colno,
-			error: {
-				message: error.message,
-				stack: error.stack,
-			},
-			from: window.location.href,
-		})
-	})
-	.then(res => {
-		document.body.innerHTML = `
-		<h1>There was an error while runnig the app</h1>
-		<h2>The error has been reported</h2>
-		`
-	})
-	.catch(err => [
-		document.body.innerHTML = `
-		<h1>There was an error while running the app</h1>
-		<h2>The error could not be reported, please send the following information to your web master</h2>
-		<code>${error}</code>
-		`
-	])
-}
-
-
 // State
 const state = {
 	location: location.state,
